e5cc9cb682e6124d7d4f1b6159159940e4b92002
Cleyson Lima <cleysonph@gmail.com>
2022-10-04 22:30:32 -0300

    02:03 - operações com arrays

--- AulaArrays.cs
+++ AulaArrays.cs
@@ -4,15 +4,28 @@ public class AulaArrays
 {
     public static void main()
     {
-        var alunos = new string[] { "Cleyson", "Elton", "Ana", "Gustavo", "Sara", "Wesley" };
+        var aluno1 = "Cleyson";
+        var aluno2 = "Elton";
+        var alunos = new string[] { aluno1, aluno2 };
 
+        imprimir(alunos);
 
-        // alunos[0] = "Cleyson";
-        // alunos[1] = "Elton";
-        // alunos[2] = "Ana";
-        // alunos[3] = "Gustavo";
-        // alunos[4] = "Sara";
-        Console.WriteLine(alunos.Length);
+        Console.WriteLine("-------------------------");
+        aluno1 = "Wesley";
+        imprimir(alunos);
+
+        Console.WriteLine("-------------------------");
+        var alunos2 = alunos;
+        imprimir(alunos2);
+        alunos[0] = "Wesley";
+        imprimir(alunos2);
+
+        Console.WriteLine("-------------------------");
+        var alunosCopia = new string[2];
+        Array.Copy(alunos, alunosCopia, alunos.Length);
+        imprimir(alunosCopia);
+        alunos[1] = "Cleyson";
+        imprimir(alunosCopia);
     }
 
     private static void imprimir(string[] array)

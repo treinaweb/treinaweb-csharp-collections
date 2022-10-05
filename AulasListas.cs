dfc491bb4f7a958c7e597934dfb45a9c5a7076e3
Cleyson Lima <cleysonph@gmail.com>
2022-10-04 23:18:00 -0300

    03:05 - operações com listas

--- AulasListas.cs
+++ AulasListas.cs
@@ -5,23 +5,40 @@ public class AulasListas
     public static void main()
     {
         var alunos = new List<string>() { "Cleyson", "Elton", "Wesley", "Ana", "Ariel" };
+        Console.WriteLine(alunos.IndexOf("Cleyson"));
+        Console.WriteLine(alunos.IndexOf("Sara"));
+
+        Console.WriteLine("-------------------------------");
+
         imprimir(alunos);
-        Console.WriteLine(alunos.Count);
-        Console.WriteLine(alunos.Capacity);
-
-        // alunos.Clear();
-        // imprimir(alunos);
-        // Console.WriteLine(alunos.Count);
-        // Console.WriteLine(alunos.Capacity);
-
-        Console.WriteLine(alunos.Contains("Sara"));
-
-        Console.WriteLine(alunos.Remove("Elton"));
+        alunos.Add("Sara");
         imprimir(alunos);
-        Console.WriteLine(alunos.Remove("Elton"));
 
-        Console.WriteLine(alunos.First());
-        Console.WriteLine(alunos.Last());
+        Console.WriteLine("-------------------------------");
+
+        alunos.Insert(2, "Amauri");
+        imprimir(alunos);
+
+        Console.WriteLine("-------------------------------");
+
+        alunos.RemoveAt(2);
+        imprimir(alunos);
+
+        Console.WriteLine("-------------------------------");
+
+        alunos.AddRange(new List<string> { "Sara", "Amauri", "Felipe" });
+        imprimir(alunos);
+
+        Console.WriteLine("-------------------------------");
+
+        Console.WriteLine(alunos.Exists(aluno => aluno.StartsWith("C")));
+
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine(alunos.Find(aluno => aluno.StartsWith("C")));
+
+        Console.WriteLine("-------------------------------");
+        var alunos2 = alunos.GetRange(0, alunos.Count);
+        imprimir(alunos2);
     }
 
     private static void imprimir(List<string> lista)
